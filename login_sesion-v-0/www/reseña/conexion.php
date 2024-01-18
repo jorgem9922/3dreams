31af4b2ce282e0b76d07449db3c630c820b2fa2a
jorgem9922 <jorgemiracerda@gmail.com>
2024-01-18 12:00:36 +0100

    Alex

--- login_sesion-v-0/www/reseña/conexion.php
+++ login_sesion-v-0/www/reseña/conexion.php
@@ -6,7 +6,13 @@ $servidor = "db";
 $usuario = "root";
 $password ="test";
 
-$conexion = mysqli_connect($servidor, $usuario, $password) or die ("Error de conexión");
+//$conexion = mysqli_connect($servidor, $usuario, $password) or die ("Error de conexión");
 
 
+/* Desarrollado por: PROGRAMANDO BROTHERS 	
+	Suscribete a : https://www.youtube.com/ProgramandoBrothers y comparte los vídeos.
+	Recuerda: "EL CONOCIMIENTO SE COMPARTE, POR MÁS POCO QUE SEA".
+	*/
+$conn = mysqli_connect('db','root','test') or die("Error, conexion");
+$bd = mysqli_select_db($conn,'productosbd') or die("Error, Base de datos");
 ?>
