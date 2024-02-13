1662101dc5f466659d6b6b4b1187bc4de62ee8a1
jorgem9922 <jorgemiracerda@gmail.com>
2024-02-13 11:18:22 +0100

    alba

--- login_sesion-v-0/www/login/index.php
+++ login_sesion-v-0/www/login/index.php
@@ -22,12 +22,12 @@
 					<input type="checkbox" class="checkbox" id="remember_me">
 					<label for="remember_me">Remember me</label>
 
-<<<<<<< HEAD
-					<input type="submit" class="button" value="Login"><a href="listado.php">
-=======
-					<input type="submit" class="button" value="Login">
 
->>>>>>> e5aa674b547352888bee5fb4abf1d4a2726e40fd
+					<input type="submit" class="button" value="Login"><a href="../index.php">
+
+					
+
+
 				</form><!--.login-form-->
 				<div class="help-text">
 					<p>Forget your password?</a></p>
