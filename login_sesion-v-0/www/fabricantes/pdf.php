da1962dfed237369d06077883078ef42faba12ad
jorgem9922 <jorgemiracerda@gmail.com>
2024-02-14 11:39:54 +0100

    AAAAAAA

--- login_sesion-v-0/www/fabricantes/pdf.php
+++ login_sesion-v-0/www/fabricantes/pdf.php
@@ -1,7 +1,7 @@
 <?php
 include_once("../conexion.php");
 //$sql = "SELECT id, employee_name, employee_salary, employee_age FROM employee LIMIT 10";          
-$sql= "SELECT * FROM `fabricante` r INNER JOIN fabricante f ON r.id_usuario = u.id_usuario INNER JOIN producto d ON r.id_producto = d.id_producto ORDER BY id_fabricante ASC LIMIT 1";
+$sql= "SELECT * FROM `fabricante` ASC LIMIT 1";
 
 mysqli_select_db($conn,"productosbd");
 
@@ -11,12 +11,12 @@ $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($c
 require('../fpdf/fpdf.php');
 $pdf = new FPDF();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',8);
+$pdf->SetFont('Arial','B',10);
 while ($field_info = mysqli_fetch_field($resultset)) {
 $pdf->Cell(17,20,$field_info->name,1);
 }
 while($rows = mysqli_fetch_assoc($resultset)) {
-$pdf->SetFont('Arial','',8);
+$pdf->SetFont('Arial','',10);
 $pdf->Ln();
 foreach($rows as $column) {
 $pdf->Cell(17,20,$column,1);
