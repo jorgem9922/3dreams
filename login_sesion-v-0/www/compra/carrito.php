9b641ae75ae8fb12ac263ff8a8f8bf6ddc523426
jorgem9922 <jorgemiracerda@gmail.com>
2024-02-15 09:30:54 +0100

    d

--- login_sesion-v-0/www/compra/carrito.php
+++ login_sesion-v-0/www/compra/carrito.php
@@ -8,18 +8,16 @@ if (!isset($_SESSION['nombre']) || $_SESSION['nombre'] === null) {
 }
 mysqli_select_db($conn, "productosbd");
 $usuario = $_SESSION['nombre'];
-// $cantidad= $_POST["cantidad"];
 $sql = "SELECT id_usuario FROM usuario where nombre='$usuario' ";
 $res = mysqli_query($conn,$sql);
 $fila = mysqli_fetch_assoc($res);
-// echo $cantidad;
     // Obtiene el id del usuario de la fila obtenida
     $id_usuario = $fila['id_usuario'];
-   
+    echo $id_usuario;
 // Obtén el id del material a eliminar
 $id = mysqli_real_escape_string($conn, $_GET["id"]);
 // Ahora puedes eliminar el registro en la tabla `material`
-$insertar = "INSERT INTO `carrito` (`id_producto`, id_usuario) VALUES ($id,$id_usuario)";
+$insertar = "INSERT INTO `carrito` (`id_producto`, id_usuario) VALUES ($id,$id_usuario')";
 mysqli_query($conn, $insertar);
 
 
