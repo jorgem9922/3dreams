3c83c137dde271e51a13432ed8f1e4a8ad0a982e
jorgem9922 <jorgemiracerda@gmail.com>
2024-02-15 09:41:01 +0100

    ttt

--- login_sesion-v-0/www/compra/producto_especifico.php
+++ login_sesion-v-0/www/compra/producto_especifico.php
@@ -17,7 +17,7 @@ $usuario = $_SESSION['nombre'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>3Dreams</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="estilo2.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
 </head>
